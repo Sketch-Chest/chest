11839f6d9be410a97d2940c9520dad2dbb928f36
Yasuaki Uechi <uetchy@randompaper.co>
2015-03-10 21:18:43 +0900

    Tidy up spec and fixtures

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe Chest::Config do
   it 'can create instance' do
-    path = '.chestrc'
-    File.delete(path) if File.exist?(path)
+    Dir.mktmpdir do |tmpdir|
+      path = File.join tmpdir, '.chestrc'
 
-    config = Chest::Config.new(path)
-    expect(config.to_hash).to eq({})
-    config.test = 1
-    expect(config.to_hash).to eq({test: 1})
-    config.save
-    expect(File.open(path).read.strip).to eq(config.to_json)
+      config = Chest::Config.new(path)
+      expect(config.to_hash).to eq({})
+      config.test = 1
+      expect(config.to_hash).to eq({test: 1})
+      config.save
+      expect(File.open(path).read.strip).to eq(config.to_json)
+    end
   end
 end
