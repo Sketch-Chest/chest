4bce31b43f607558b98857a8ec23f7cf5403c093
Yasuaki Uechi <uetchy@randompaper.co>
2015-03-22 18:55:08 +0900

    Change method name

--- lib/chest/helpers/zipfile.rb
+++ lib/chest/helpers/zipfile.rb
@@ -7,26 +7,28 @@ class ZipFileGenerator
     @ignoredFiles = ignoredFiles
   end
 
-  def write()
+  def write
     entries = Dir.entries(@inputDir)
     @ignoredFiles.each{|e| entries.delete(e) }
     io = Zip::File.open(@outputFile, Zip::File::CREATE)
-    writeEntries(entries, "", io)
-    io.close()
+    write_entries(entries, '', io)
+    io.close
   end
 
   private
 
-  def writeEntries(entries, path, io)
-    entries.each do |e|
-      zipFilePath = path == "" ? e : File.join(path, e)
+  def write_entries(entries, path, io)
+    entries.each do |entry|
+      zipFilePath = path == '' ? entry : File.join(path, entry)
       diskFilePath = File.join(@inputDir, zipFilePath)
       if File.directory?(diskFilePath)
         io.mkdir(zipFilePath)
-        subdir =Dir.entries(diskFilePath); subdir.delete("."); subdir.delete("..")
-        writeEntries(subdir, zipFilePath, io)
+        subdir = Dir.entries(diskFilePath)
+        subdir.delete('.')
+        subdir.delete('..')
+        write_entries(subdir, zipFilePath, io)
       else
-        io.get_output_stream(zipFilePath) { |f| f.print(File.open(diskFilePath, "rb").read())}
+        io.get_output_stream(zipFilePath) { |f| f.print(File.open(diskFilePath, 'rb').read) }
       end
     end
   end
